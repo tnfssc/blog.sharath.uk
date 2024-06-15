0475fae4041c5d1f84fb78fff1b101f83f0c90fd
sharath <29162020+tnfssc@users.noreply.github.com>
2024-06-15 17:48:47 +0530

    fix(images): removed unpic astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import { imageService } from '@unpic/astro/service';
-import { defineConfig } from 'astro/config';
 import Icons from 'unplugin-icons/vite';
+
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://blog.sharath.uk',
   output: 'static',
   integrations: [mdx(), sitemap(), tailwind()],
-  image: { service: imageService() },
   vite: { plugins: [Icons({ compiler: 'astro' })] },
 });
