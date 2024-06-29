fea7f579f5a755745e0ec5ab234ecdd275db05b9
sharath <29162020+tnfssc@users.noreply.github.com>
2024-06-29 19:01:50 +0530

    feat(rss.json): added json version of RSS feed

---
+++ src/pages/rss.json.ts
@@ -0,0 +1,20 @@
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+
+export async function GET(context: APIContext) {
+  if (!context.site) throw new Error('No site found');
+
+  const posts = await getCollection('blog');
+  return {
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      pubDate: post.data.date,
+      link: `/blog/${post.slug}/`,
+    })),
+  };
+}
