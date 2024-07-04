3548c834b5c4e60006a92bfd25dbab3785ee6414
sharath <29162020+tnfssc@users.noreply.github.com>
2024-07-04 11:48:31 +0530

    feat(icons): added icon support for yaml and env

--- src/lib/lang-icon.ts
+++ src/lib/lang-icon.ts
@@ -5,7 +5,9 @@ import DeviconPlainJson from '~icons/devicon-plain/json';
 import DeviconPlainPostcss from '~icons/devicon-plain/postcss';
 import DeviconPlainRust from '~icons/devicon-plain/rust';
 import DeviconPlainTypescript from '~icons/devicon-plain/typescript';
+import DeviconPlainYaml from '~icons/devicon-plain/yaml';
 import MaterialSymbolsMarkdown from '~icons/material-symbols/markdown';
+import SimpleIconsDotenv from '~icons/simple-icons/dotenv';
 import SimpleIconsMdx from '~icons/simple-icons/mdx';
 
 export const LangIcons = {
@@ -19,6 +21,8 @@ export const LangIcons = {
   mdx: SimpleIconsMdx,
   md: MaterialSymbolsMarkdown,
   json: DeviconPlainJson,
+  env: SimpleIconsDotenv,
+  yaml: DeviconPlainYaml,
 };
 
 export type Language = keyof typeof LangIcons;
