b3734f3f99f7d42b0e09b65f635d271fa63c98da
sharath <29162020+tnfssc@users.noreply.github.com>
2024-07-04 11:46:36 +0530

    feat(copy): improved code copy button

--- src/components/CodeHeadCopy.react.tsx
+++ src/components/CodeHeadCopy.react.tsx
@@ -1,13 +1,34 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-export const CodeHeadCopyReact: React.FC<React.HTMLAttributes<HTMLButtonElement>> = (props) => {
+export const CodeHeadCopyReact: React.FC<
+  React.HTMLAttributes<HTMLButtonElement> & { copiedIcon?: React.ReactNode }
+> = ({ children, copiedIcon = children, ...props }) => {
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (copied) {
+        setCopied(false);
+      }
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [copied]);
+
   const handleClick = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
     const codeBlock = e.currentTarget.parentElement?.parentElement?.nextElementSibling?.querySelector('code');
     if (codeBlock?.textContent) {
       const text = codeBlock.textContent;
       void navigator.clipboard.writeText(text);
+      setCopied(true);
     }
   }, []);
 
-  return <button onMouseDown={handleClick} {...props} />;
+  return (
+    <button onMouseDown={handleClick} {...props}>
+      {copied ? copiedIcon : children}
+    </button>
+  );
 };
