7038b2e49305a5f8db84f68286f26e7f3e36e6cd
sharath <29162020+tnfssc@users.noreply.github.com>
2024-07-06 00:06:19 +0530

    fix(img): better imgs for image transitions

---
+++ src/lib/postImgs.ts
@@ -0,0 +1,14 @@
+import type { GetImageResult } from 'astro';
+import { getImage } from 'astro:assets';
+import { getCollection } from 'astro:content';
+
+export type PostImgs = Record<string, GetImageResult>;
+
+export const postImgs = async () => {
+  const posts = await getCollection('blog');
+  const rawImgs = posts.map((p) => p.data.heroImage);
+  const imgs = await Promise.all(rawImgs.map((i) => getImage({ src: i, width: 800, height: 500 })));
+  const postImgs = new Map(imgs.map((i, idx) => [posts[idx]!.slug, i]));
+  const postImgsObj: PostImgs = Object.fromEntries(postImgs);
+  return postImgsObj;
+};
--- src/pages/img.o.json.ts
+++ src/pages/img.o.json.ts
@@ -1,16 +1,10 @@
 import type { GetImageResult } from 'astro';
-import { getImage } from 'astro:assets';
-import { getCollection } from 'astro:content';
+
+import { postImgs } from '@/lib/postImgs';
 
 export type PostImgs = Record<string, GetImageResult>;
 
 export async function GET() {
-  const posts = await getCollection('blog');
-  const rawImgs = posts.map((p) => p.data.heroImage);
-  const imgs = await Promise.all(rawImgs.map((i) => getImage({ src: i, width: 800, height: 500 })));
-  const postImgs = new Map(imgs.map((i, idx) => [posts[idx]!.slug, i]));
-
-  const postImgsObj: PostImgs = Object.fromEntries(postImgs);
-
+  const postImgsObj = await postImgs();
   return new Response(JSON.stringify(postImgsObj));
 }
